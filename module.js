f9fc021b1d46cbdecd72b131d3d3129dc9795177
Kevin Huelsmann <kjh311@gmail.com>
2016-10-19 10:06:22 -0700

    angular factory works

--- module.js
+++ module.js
@@ -1,6 +1,22 @@
 var app = angular.module("myApp", []);
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", ['$scope', 'userService', function($scope, userService) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
-});
+
+    $scope.doIt = function(){
+      userService.firstFunction();
+    }
+}]);
+
+
+
+app.factory('userService', [function(){
+
+var blah = {};
+  blah.firstFunction = function () {
+    alert('userService through factory!');
+  };
+
+return blah;
+}]);
